0344e26165e31a627a0697513b640a27aa1d8551
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-12-12 13:46:26 +0100

    functional logout with reactive layout depending on user

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
+
+import { signOut } from "@firebase/auth";
+import { useCurrentUser } from "vuefire";
+import { auth } from "@/plugins/firebase";
+
 const router = useRouter();
+
+const user = useCurrentUser();
+
+const logout = async () => {
+  await signOut(auth);
+};
 </script>
 
 <template>
@@ -11,7 +22,20 @@ const router = useRouter();
       <v-spacer></v-spacer>
 
       <v-btn @click="router.push('/')" variant="flat">Home</v-btn>
-      <v-btn @click="router.push('/login')" variant="flat">Login</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <template v-if="user">
+        {{ user.displayName }}
+        <v-btn @click="router.push('/dashboard')" variant="flat"
+          >Dashboard</v-btn
+        >
+        <v-btn @click="logout" variant="flat">Logout</v-btn>
+      </template>
+      <template v-else>
+        <v-btn @click="router.push('/login')" variant="flat">Login</v-btn>
+        <v-btn @click="router.push('/register')" variant="flat">Register</v-btn>
+      </template>
     </v-app-bar>
 
     <v-main>
