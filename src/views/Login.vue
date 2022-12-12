1f8fd223a6feec12b3ea78854d273ae2e90a2548
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-12-12 13:34:50 +0100

    fix typos password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <h1>Login</h1>
   <v-form>
     <v-text-field type="email" placeholder="john.doe@mail.com" />
-    <v-text-field type="passoword" placeholder="******" />
+    <v-text-field type="password" placeholder="******" />
     <v-btn block>submit</v-btn>
   </v-form>
 </template>
