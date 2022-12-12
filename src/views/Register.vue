4550c75542a7924d4a06446d863bc7d5d7e0848c
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-12-12 13:35:56 +0100

    add register page

---
+++ src/views/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Register</h1>
+  <v-form>
+    <v-text-field type="text" placeholder="John" />
+    <v-text-field type="text" placeholder="Doe" />
+    <v-text-field type="email" placeholder="john.doe@mail.com" />
+    <v-text-field type="password" placeholder="******" />
+    <v-btn block>submit</v-btn>
+  </v-form>
+</template>
