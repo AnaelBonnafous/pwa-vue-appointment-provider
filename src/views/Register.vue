8270cb902d821e370ca8849158738b4cb0561b2c
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-12-12 13:46:13 +0100

    functional register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
 import { reactive } from "vue";
 
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import { updateCurrentUserProfile } from "vuefire";
+import { auth } from "@/plugins/firebase";
+
 const form = reactive({
   firstname: "",
   lastname: "",
   email: "",
   password: "",
 });
+
+const register = async () => {
+  try {
+    await createUserWithEmailAndPassword(auth, form.email, form.password);
+
+    await signInWithEmailAndPassword(auth, form.email, form.password);
+
+    await updateCurrentUserProfile({
+      displayName: `${form.firstname} ${form.lastname}`,
+    });
+  } catch (error) {
+    //
+  }
+};
 </script>
 
 <template>
   <h1>Register</h1>
-  <v-form>
+  <v-form @submit.prevent="register">
     <v-text-field v-model="form.firstname" type="text" placeholder="John" />
     <v-text-field v-model="form.lastname" type="text" placeholder="Doe" />
     <v-text-field
@@ -24,6 +45,6 @@ const form = reactive({
       type="password"
       placeholder="******"
     />
-    <v-btn block>submit</v-btn>
+    <v-btn type="submit" block>submit</v-btn>
   </v-form>
 </template>
